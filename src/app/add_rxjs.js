bd819f0ebd11ab5ef45f9e1ee2e8e6bdf539f002
zsofiap <116664587+zsofiap@users.noreply.github.com>
2023-01-31 15:11:51 +0100

    rxjs back

--- src/app/add_rxjs.js
+++ src/app/add_rxjs.js
@@ -1,10 +1,14 @@
 import * as RxJS from 'rxjs';
 import * as RxJSOperators from 'rxjs/operators';
 
-export class WidgetContext {
-      rxjs = {
-        ...RxJS,
-        ...RxJSOperators
-      };
-  }
+// export class WidgetContext {
+//       rxjs = {
+//         ...RxJS,
+//         ...RxJSOperators
+//       };
+//   }
 
+rxjs = {
+          ...RxJS,
+          ...RxJSOperators
+        };
